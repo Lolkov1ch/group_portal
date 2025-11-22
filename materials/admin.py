eb7f450012c45fbadadb33109bb90a7a6617a14f
fand1l <fand1l@outlook.com>
2025-11-22 12:32:43 +0200

    MAT-1 - Add Material model and registered in admin

--- materials/admin.py
+++ materials/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Material
 
 # Register your models here.
+@admin.register(Material)
+class MaterialAdmin(admin.ModelAdmin):
+    list_display = ("name", "file_type", "file_link", "subject", "author", "created_at")
+    list_filter = ("file_type",)
+    search_fields = ("name", "author__username")
+    readonly_fields = ("created_at",)
--- materials/models.py
+++ materials/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Material(models.Model):
+    class FileTypeChoices(models.TextChoices):
+        FILE = "file", "File"
+        LINK = "link", "Link"
+        VIDEO = "video", "Video"
+
+    name = models.CharField(max_length=128)
+    description = models.CharField(max_length=1024, blank=True)
+    file_type = models.CharField(
+        max_length=16,
+        choices=FileTypeChoices.choices
+    )
+    file_link = models.CharField(max_length=512)
+    subject = models.CharField(max_length=128)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
