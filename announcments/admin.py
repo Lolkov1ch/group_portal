275a61f8d3dfe05b8a2a7f43aabd0857870f8f9d
D6 <konctantun68@gmail.com>
2025-11-22 12:26:18 +0200

    ANN-1 - створення моделі Announcement та реєстрація в адмінці

--- announcments/admin.py
+++ announcments/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Announcement
 
 # Register your models here.
+
+@admin.register(Announcement)
+class AnnouncementAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "published_date", "is_important")
+    list_filter = ("is_important", "published_date")
+    search_fields = ("title", "text", "author__username")
--- announcments/models.py
+++ announcments/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Announcement(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Заголовок")
+    text = models.TextField(verbose_name="Текст")
+    published_date = models.DateTimeField(auto_now_add=True, verbose_name="Дата публікації")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Автор")
+    is_important = models.BooleanField(default=False, verbose_name="Важливе")
+
+    class Meta:
+        verbose_name = "Оголошення"
+        verbose_name_plural = "Оголошення"
+        ordering = ['-published_date']
+
+    def __str__(self):
+        return f"{self.title} ({'Важливе' if self.is_important else 'Звичайне'})"
+
+    def short_text(self):
+        return self.text[:50] + "..." if len(self.text) > 50 else self.text
