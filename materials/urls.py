ef50d757a41fa45aaa07e80e4464561f7bd2ab1f
fand1l <fand1l@outlook.com>
2025-12-04 22:21:36 +0200

    MAT-2.2 - implement Material List/Detail views with filters and some icons

--- materials/urls.py
+++ materials/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import CreateMaterial
+from .views import CreateMaterial, ListMaterial, DetailMaterial
 
 app_name = "materials"
 
 urlpatterns = [
-    path("create/", CreateMaterial.as_view(), name="material_create")
+    path("create/", CreateMaterial.as_view(), name="material_create"),
+    path("", ListMaterial.as_view(), name="material_list"),
+    path("<int:pk>/", DetailMaterial.as_view(), name="material_detail")
 ]
--- materials/views.py
+++ materials/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
-from .models import Material
+from .models import Material, MaterialCategory
 from .forms import MaterialCreateForm
+from django.http import HttpResponseRedirect
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 class CreateMaterial(LoginRequiredMixin, UserPassesTestMixin, CreateView):
@@ -19,9 +21,92 @@ class CreateMaterial(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     
     # Якщо користувач не має прав
     def handle_no_permission(self):
+        if self.request.user.is_authenticated:
+            return super().handle_no_permission()  # 403
         return redirect("login")
 
     # Перевірка валідності форми
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+    
+
+class ListMaterial(ListView):
+    model = Material
+    template_name = "materials/material_list.html"
+    context_object_name = "materials"
+    paginate_by = 10
+
+    def get(self, request, *args, **kwargs):
+        # Якщо немає поточних GET-фільтрів, але є збережені в сесії — редірект з ними
+        if not (request.GET.get('category') or request.GET.get('engine') or request.GET.get('type')):
+            filters = request.session.get("material_filters")
+            if filters and (filters.get("category") or filters.get("engine") or filters.get("type")):
+                params = []
+                if filters.get("category"):
+                    params.append(f"category={filters['category']}")
+                if filters.get("engine"):
+                    params.append(f"engine={filters['engine']}")
+                if filters.get("type"):
+                    params.append(f"type={filters['type']}")
+                redirect_url = f"{reverse_lazy('material_list')}?{'&'.join(params)}"
+                return HttpResponseRedirect(redirect_url)
+
+        return super().get(request, *args, **kwargs)
+    
+
+    def get_queryset(self):
+         # Показувати лише опубліковані матеріали
+        queryset = Material.objects.filter(is_published=Material.IsPublishedChoices.PUBLISHED)
+
+        category_id = self.request.GET.get('category')
+        engine = self.request.GET.get('engine')
+        file_type = self.request.GET.get('type')
+
+        # Фільтри
+        if category_id:
+            queryset = queryset.filter(category__id=category_id)
+
+        if engine:
+            queryset = queryset.filter(engine=engine)
+
+        if file_type:
+            queryset = queryset.filter(file_type=file_type)
+
+        # Збереження фільтрів у сесії
+        if category_id or engine or file_type:
+            self.request.session["material_filters"] = {
+                "category": category_id,
+                "engine": engine,
+                "type": file_type,
+            }
+        
+        elif "material_filters" in self.request.session:
+            del self.request.session["material_filters"] # Видалення збережених фільтрів
+
+        return queryset
+    
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context["categories"] = MaterialCategory.objects.all()
+        context["engines"] = Material.EngineChoices.choices
+        context["file_types"] = Material.FileTypeChoices.choices
+
+        context['active_category'] = self.request.GET.get('category')
+        context['active_engine'] = self.request.GET.get('engine')
+        context['active_type'] = self.request.GET.get('type')
+
+        get_params = self.request.GET.copy()
+        if 'page' in get_params:
+            del get_params['page']
+        context['get_params_except_page'] = get_params.urlencode()
+        
+        return context
+
+
+class DetailMaterial(DetailView):
+    model = Material
+    template_name = "materials/material_detail.html"
+    context_object_name = "material"
