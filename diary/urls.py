5ada5b7d14305ec1560732cb4ea92bd8a54c95c0
fand1l <fand1l@outlook.com>
2025-12-13 12:07:13 +0200

    DIA-2 - add teacher gradebook functionality

--- diary/urls.py
+++ diary/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
+from .views import GradeListView, GradeCreateView, GradeUpdateView, GradeDeleteView
 
 app_name = "diary"
 
-urlpatterns = []
+urlpatterns = [
+    path('grades/', GradeListView.as_view(), name='grade_list'),
+    path('grades/add/', GradeCreateView.as_view(), name='grade_create'),
+    path('grades/<int:pk>/edit/', GradeUpdateView.as_view(), name='grade_update'),
+    path('grades/<int:pk>/delete/', GradeDeleteView.as_view(), name='grade_delete'),
+]
