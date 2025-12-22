80f9e19ecce87f6449e35e9b16ff181a3c5193ee
Vasil <vasamuhanuk@gmail.com>
2025-12-22 20:03:41 +0200

    створений список

--- surveys/views.py
+++ surveys/views.py
@@ -1,14 +1,29 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
+from django.db.models import Q
+from django.utils import timezone
+from .models import Survey
 
 # Create your views here.
 
-class SurveysListView(TemplateView):
+class SurveysListView(ListView):
     """Список всіх опитувань"""
+    model= Survey
     template_name = 'surveys/surveys_list.html'
+    context_object_name = 'surveys'
+    def get_queryset(self):
+        now = timezone.now()
+
+        return Survey.objects.filter(
+            is_active=True
+        ).filter(
+            Q(start_date_lte=now) | Q (start_date__isnull=True)
+        ).filters(
+            Q(end_date__gte=now) | Q(end_date__isnull=True)
+        ).order_by('-start_date', '-create_at')
 
 
-class SurveyDetailView(TemplateView):
+class SurveyDetailView(DetailView):
     """Деталі конкретного опитування"""
     template_name = 'surveys/survey_detail.html'
 
@@ -16,3 +31,4 @@ class SurveyDetailView(TemplateView):
 class SurveyStepView(TemplateView):
     """Крок опитування"""
     template_name = 'surveys/survey_step.html'
+
